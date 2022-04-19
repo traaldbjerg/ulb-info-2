9f6b17f6c286db350397c2d4c7bf07791c6e0962
traaldbjerg <73064935+traaldbjerg@users.noreply.github.com>
2022-04-19 17:34:19 +0200

    Update MainActivity.kt
    J'avais oublié de bien copier le main activity du jeu canon

--- AngryProfs/app/src/main/java/com/example/angryprofs/MainActivity.kt
+++ AngryProfs/app/src/main/java/com/example/angryprofs/MainActivity.kt
@@ -3,9 +3,19 @@ package com.example.angryprofs
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
+    lateinit var canonView: CanonView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        canonView = findViewById<CanonView>(R.id.vMain)
+    }
+    override fun onPause() {
+        super.onPause()
+        canonView.pause()
+    }
+    override fun onResume() {
+        super.onResume()
+        canonView.resume()
     }
 }
