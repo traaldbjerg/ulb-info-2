ce7ab6fd432e71b90dfdccbd3b92ccd9f3db25e5
traaldbjerg <73064935+traaldbjerg@users.noreply.github.com>
2022-04-27 21:45:43 +0200

    Update Prof.kt

--- AngryProfs/app/src/main/java/com/example/angryprofs/Prof.kt
+++ AngryProfs/app/src/main/java/com/example/angryprofs/Prof.kt
@@ -1,76 +1,82 @@
 package com.example.angryprofs
 
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.PointF
+import android.graphics.*
 
-class Prof(var view: CanonView, val obstacle: Obstacle, val cible: Cible, var prof : Int) {
-    var profP = PointF()
-    var profVitesse = 0f
-    var profVitesseX = 0f
-    var profVitesseY = 0f
+open class Prof(var x: Float, var y: Float, val width: Float, var view: CanonView, val obstacle: Obstacle, val cible: Cible, var prof : Int) {
+    var r = RectF(x, y, x + width, y + width * 1.2f)
+    var v0 = 0f
+    var vx = 0f
+    var vy = 0f
     var profOnScreen = true
     var profSize = 0f
-    var profPaint = Paint()
+    var raint = Paint()
     var currentHP : Int = 0
     val hpList = listOf(4,4,4,4)
     val nameList = listOf("Marc Haelterman", "Jean-Marc Sparenberg", "Hugues Bersini", "Philippe Bogaerts") //probablement a rajouter dans un string.xml
+    val image =
+        view.getResources().getIdentifier("NOMDUPROF", "drawable", view.getContext().getPackageName())
+
+    //pour plus general, mettre le nom de la ressource en attribut, facilement changeable a la creation d'objet
+    val bmp = BitmapFactory.decodeResource(view.getResources(), image)
 
     val gravity = 1800
 
     init {
-        profPaint.color = Color.RED
+        raint.color = Color.RED
     }
 
     fun launch(angle: Double) {
-        profP.x = profSize
-        profP.y = view.screenHeight / 2f
-        profVitesseX = (profVitesse * Math.sin(angle)).toFloat()
-        profVitesseY = (-profVitesse * Math.cos(angle)).toFloat()
+        x = profSize
+        y = view.screenHeight / 2f
+        vx = (v0 * Math.sin(angle)).toFloat()
+        vy = (-v0 * Math.cos(angle)).toFloat()
         profOnScreen = true
     }
 
     fun draw(canvas: Canvas) {
-        canvas.drawCircle(
-            profP.x, profP.y, profSize,
-            profPaint
-        )
+        canvas.drawBitmap(bmp, null, r, null)
     }
 
     fun update(interval: Double) {
         if (profOnScreen) {
-            profVitesseY += (interval * (gravity)).toFloat()
-            profP.x += (interval * profVitesseX).toFloat()
-            profP.y += (interval * profVitesseY).toFloat()
+            vy += (interval * (gravity)).toFloat()
+            r = RectF(x, y, x + width, y + width * 1.2f)
+            r.offset((interval * vx).toFloat(), (interval * vy).toFloat())
+            x += (interval * vx).toFloat()
+            y += (interval * vy).toFloat()
+
             /* Vérifions si la balle touche l'obstacle ou pas */
-            if (profP.x + profSize > obstacle.obstacle.left && profP.x - profSize < obstacle.obstacle.right
-                && profP.y + profSize > obstacle.obstacle.top
-                && profP.y - profSize < obstacle.obstacle.bottom
+            if (x + profSize > obstacle.obstacle.left && x - profSize < obstacle.obstacle.right
+                && y + profSize > obstacle.obstacle.top
+                && y - profSize < obstacle.obstacle.bottom
             ) {
-                profVitesseX *= -1
-                profP.offset((3 * profVitesseX * interval).toFloat(), 0f)
+                vx *= -1
+                r.offset((3 * vx * interval).toFloat(), 0f)
                 view.reduceTimeLeft()
                 view.playObstacleSound()
             }
             // Si elle sort de l'écran
-            else if (profP.x + profSize > view.screenWidth
-                || profP.x - profSize < 0
+            else if (x + profSize > view.screenWidth
+                || x - profSize < 0
             ) {
                 profOnScreen = false
-            } else if (profP.y + profSize > view.screenHeight
-                || profP.y - profSize < 0
+            } else if (y + profSize > view.screenHeight
+                || y - profSize < 0
             ) {
                 profOnScreen = false
-            } else if (profP.x + profSize > cible.cible.left
-                && profP.y + profSize > cible.cible.top
-                && profP.y - profSize < cible.cible.bottom
+            } else if (x + profSize > cible.cible.left
+                && y + profSize > cible.cible.top
+                && y - profSize < cible.cible.bottom
             ) {
                 cible.detectChoc(this)
 
                 /* Pour l'instant rien ne se passe lorsque balle heurte la cible */
             }
         }
+    }
+
+    open fun MyMove() {
+
     }
 
     fun resetProf() {
